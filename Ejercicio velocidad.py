722c8634029424c37ea1870db47de132cf77b1c3
Iván <46821602+VictorIvanMr@users.noreply.github.com>
2019-01-22 11:05:18 -0600

    Víctor Iván Morales Ramos//Grupo #03

---
+++ Ejercicio velocidad.py
@@ -0,0 +1,14 @@
+#Autor: Víctor Iván Morales Ramos#
+#Calcula la velocidad promedio, dado el tiempo y la distancia
+#A01377601
+
+d = int(input("Dame la distancia de tu viaje en Km enteros: "))
+t = int(input("Dame el tiempo de tu viaje en Horas enteras: "))
+
+#----------------------------------------------------------------------------------------------------------------------#
+
+v = d / t
+
+#----------------------------------------------------------------------------------------------------------------------#
+
+print("Tu velocidad promedio es: ", v , "km/h")
